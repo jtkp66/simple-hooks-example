37bcdf7e3205eea667c158fc9faf9bec4759a443
Jamie Pham <jtkp66@gmail.com>
2019-07-22 23:39:30 -0700

    added useState to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      {resource}
+    </div>
+  );
+};
+
+export default App;
